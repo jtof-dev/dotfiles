26b754963ac03254216e93337ee22887801b657d
JJTofflemire <jtof-dev@pm.me>
2024-07-10 22:47:17 -0700

    feat: add initial astronvim config

---
+++ nvim/lua/community.lua
@@ -0,0 +1,16 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.file-explorer.mini-files" },
+  -- import/override with your plugins folder
+}

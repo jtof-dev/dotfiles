a82ffe16dcd96ee927514b1a3e2f539962592125
Andy Babcock <jtof-dev@pm.me>
2024-08-19 22:41:41 -0700

    feat: tidy up dotfilesUpdate.py

--- dotfilesUpdate.py
+++ dotfilesUpdate.py
@@ -3,19 +3,39 @@
 import os
 import subprocess
 
-commands = [
-    "cp -rv ~/.config/nvim ./crossPlatform/",
-    "cp -rv ~/.config/fastfetch ./crossPlatform/",
-    "cp -rv ~/.config/zellij ./crossPlatform/",
-    "cp -v ~/.config/spicetify/CustomApps/customAppsUpdate.sh ./spicetify/CustomApps/",
-    "cp -v ~/.config/kitty/kitty.conf ./crossPlatform/",
-    "cp -v ~/.config/starship.toml ./crossPlatform/",
-    "cp -v ~/.tmux.conf ./crossPlatform/",
-    "sudo cp -rv ~/.config/yazi ./crossPlatform/",
-    "cp -v ~/git-repos/gitReposUpdate.py ./scripts/",
-    "cp -rv ~/.config/aerc ./crossPlatform/",
-    "cp -rv ~/.config/spotify-player ./crossPlatform/",
-]
+
+def main():
+    commands = [
+        "cp -rv ~/.config/nvim ./crossPlatform/",
+        "cp -rv ~/.config/fastfetch ./crossPlatform/",
+        "cp -rv ~/.config/zellij ./crossPlatform/",
+        "cp -v ~/.config/spicetify/CustomApps/customAppsUpdate.sh ./spicetify/CustomApps/",
+        "cp -v ~/.config/kitty/kitty.conf ./crossPlatform/",
+        "cp -v ~/.config/starship.toml ./crossPlatform/",
+        "cp -v ~/.tmux.conf ./crossPlatform/",
+        "sudo cp -rv ~/.config/yazi ./crossPlatform/",
+        "cp -v ~/git-repos/gitReposUpdate.py ./scripts/",
+        "cp -rv ~/.config/aerc ./crossPlatform/",
+        "cp -rv ~/.config/spotify-player ./crossPlatform/",
+    ]
+    for command in commands:
+        runCommand(command)
+
+    dotfilesOs = getOs()
+    print(f"Desktop Environment: {dotfilesOs}")
+
+    match dotfilesOs:
+        case "desktopArchlinux":
+            command = "cd ~/git-repos/dotfiles/desktopArchlinux && ./dotfiles.sh"
+            runCommand(command)
+        case "mbpMacos":
+            command = "cd ~/git-repos/dotfiles/mbpMacos && ./dotfiles.sh"
+            runCommand(command)
+        # case "mbpArchlinux":
+        # call script
+        case "unknown":
+            print("environmental variable not found. is it set?")
+            exit()
 
 
 def runCommand(command):
@@ -35,21 +55,5 @@ def getOs():
         return "unknown"
 
 
-for command in commands:
-    runCommand(command)
-
-dotfilesOs = getOs()
-print(f"Desktop Environment: {dotfilesOs}")
-
-match dotfilesOs:
-    case "desktopArchlinux":
-        command = "cd ~/git-repos/dotfiles/desktopArchlinux && ./dotfiles.sh"
-        runCommand(command)
-    case "mbpMacos":
-        command = "cd ~/git-repos/dotfiles/mbpMacos && ./dotfiles.sh"
-        runCommand(command)
-    # case "mbpArchlinux":
-    # call script
-    case "unknown":
-        print("environmental variable not found. is it set?")
-        exit()
+if __name__ == "__main__":
+    main()

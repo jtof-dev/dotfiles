0fe93f41ec6508d93fd2e70dbcb9c774de6ca145
Andy Babcock <contact@jtof.dev>
2024-12-03 17:22:01 -0700

    chore: update dots on desktopArchlinux

--- scripts/spicetifyFlatpakHook/spotifyVersionCheck.sh
+++ scripts/spicetifyFlatpakHook/spotifyVersionCheck.sh
@@ -7,8 +7,8 @@ previous_version=$(cat "$spotifyVersion")
 if [[ "$current_version" != "$previous_version" ]]; then
 	echo "spotify updated to version $current_version"
 
-	chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
-	chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps -R
+	sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
+	sudo chmod a+wr /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps -R
 	spicetify backup apply
 
 	echo "$current_version" >"$spotifyVersion"
